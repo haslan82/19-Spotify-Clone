42a056ffe7b0c4892763556273cb7ef059602ae7
haslan82 <haslan82@hotmail.com>
2024-04-23 22:41:14 +0300

    ekledimm

--- js/ui.js
+++ js/ui.js
@@ -5,6 +5,25 @@ export const renderSongs = (songs) => {
   songs.forEach((song) => {
     //* Kart datasına kart elemanına bazı verileri ekleme
     const div = document.createElement("div");
-    console.log("div");
+    div.dataset.url = song.hub?.actions?.pop().uri;
+ div.dataset.title = song.title
+ div.dataset.img = song.images?.coveart;
+
+
+ div.className = "card";
+div.innerHTML=`
+<figure>
+        <img
+        src="${song.images?.coverart}"
+        alt=""
+        />
+        <div class="play">
+        <i class="bi bi-play-fill" id="play-btn"></i>
+        </div>
+    </figure>
+    <h4>${song.subtitle}</h4>
+    <h4>${song.title}</h4>`;
+    elements.list.appendChild(div);
+
     });
 };
